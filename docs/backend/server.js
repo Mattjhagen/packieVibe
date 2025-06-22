2a6bff550fa56837321436d0c6c139dc2b853c99
Matt and Terra’s Adventures <Mattjhagen@users.noreply.github.com>
2025-06-22 10:23:33 -0500

    Update server.js

--- docs/backend/server.js
+++ docs/backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 
 dotenv.config();
 const app = express();
